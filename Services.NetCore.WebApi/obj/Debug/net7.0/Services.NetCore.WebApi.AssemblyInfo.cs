6cc93f90c8a7a43370e34f347f8a4f671d735f75
Jonathan <johnbonilla007@gmail.com>
2024-03-19 12:57:58 -0600

    reorgornizing files

--- Services.NetCore.WebApi/obj/Debug/net7.0/Services.NetCore.WebApi.AssemblyInfo.cs
+++ Services.NetCore.WebApi/obj/Debug/net7.0/Services.NetCore.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services.NetCore.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3503659001ff369ed436b5814e8cc574e43dbd95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62ee2570a0d5d3bf98bb8b8ec4b36dffbf01b682")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services.NetCore.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services.NetCore.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
